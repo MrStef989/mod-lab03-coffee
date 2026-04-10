630d9e0b02c18733b9c2f9c16ff21dbc41c255fd
MrStef989 <borodovgerman123@gmail.com>
2026-04-10 10:49:01 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,98 @@
-// Copyright 2022 GHA Test Team
-
+// Copyright 2026 MrStef989
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(AutomataTest, InitialStateIsOff) {
+    Automata a;
+    EXPECT_EQ(a.getState(), STATES::OFF);
+}
+
+TEST(AutomataTest, TurnOn) {
+    Automata a;
+    a.on();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+}
+
+TEST(AutomataTest, TurnOff) {
+    Automata a;
+    a.on();
+    a.off();
+    EXPECT_EQ(a.getState(), STATES::OFF);
+}
+
+TEST(AutomataTest, CoinChangesState) {
+    Automata a;
+    a.on();
+    a.coin(10);
+    EXPECT_EQ(a.getState(), STATES::ACCEPT);
+}
+
+TEST(AutomataTest, CoinAccumulatesCash) {
+    Automata a;
+    a.on();
+    a.coin(20);
+    a.coin(30);
+    EXPECT_EQ(a.cash, 50);
+}
+
+TEST(AutomataTest, ChoiceValid) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    EXPECT_TRUE(a.choice(0));
+}
+
+TEST(AutomataTest, ChoiceInvalid) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    EXPECT_FALSE(a.choice(99));
+}
+
+TEST(AutomataTest, CheckSuccess) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(0);
+    EXPECT_TRUE(a.check());
+    EXPECT_EQ(a.getState(), STATES::COOK);
+}
+
+TEST(AutomataTest, CheckFail) {
+    Automata a;
+    a.on();
+    a.coin(10);
+    a.choice(1);
+    EXPECT_FALSE(a.check());
+    EXPECT_EQ(a.getState(), STATES::ACCEPT);
+}
+
+TEST(AutomataTest, CookChangesState) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(0);
+    a.check();
+    a.cook();
+    EXPECT_EQ(a.getState(), STATES::DONE);
+}
+
+TEST(AutomataTest, FinishResetsState) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(0);
+    a.check();
+    a.cook();
+    a.finish();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+}
+
+TEST(AutomataTest, CancelReturnsToWait) {
+    Automata a;
+    a.on();
+    a.coin(20);
+    a.cancel();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+    EXPECT_EQ(a.cash, 0);
+}
